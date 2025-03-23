84f942a2632c4927d4f693036f951c42e60c0683
Vinay2704 <85406544+vinay95603@users.noreply.github.com>
2025-03-23 21:25:29 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,7 +13,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = "vinay";
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -21,5 +21,46 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 22;
 let stringNumber = String(someNumber);
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+// **************** Operations ***************
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " vinay"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "3" + 5 + 6)
+// console.log(1 + 2 + 3 + "4" + 5 + 6);
+
+// console.log( (3+4) * 5 % 3)
+
+// console.log(true);
+// console.log(+ true);
+
+// console.log(true+)
+
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
