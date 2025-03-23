963305235513a485e28c6f58b4d470d36ab625a4
Vinay2704 <85406544+vinay95603@users.noreply.github.com>
2025-03-23 21:19:56 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "vinay"
+// console.log(typeof score)
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn = "vinay";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "vinay" => true
+
+let someNumber = 22;
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof stringNumber);
